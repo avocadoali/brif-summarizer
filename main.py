6f87739fb069bed2b005028998d68a7db80620a2
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-12-23 00:14:25 +0100

    automated summary process (basic asf)

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 
 import ebooklib
+import sys
+import os
 from ebooklib import epub
 from bs4 import BeautifulSoup
+from summarize import summarize
 
 
 blacklist = [   '[document]',   'noscript', 'header',   'html', 'meta', 'head','input', 'script',   ]
@@ -39,8 +42,83 @@ def epub2text(epub_path):
     ttext = thtml2ttext(chapters)
     return ttext
 
-out=epub2text('./Godfather.epub')
-f = open("demofile.txt", "a")
-for o in out:
-    f.write(o)
-f.close()
+def empty_sandbox():
+    print("Emptying sandbox directory...")
+    for f in os.listdir("Sandbox/"):
+        os.remove("Sandbox/" + str(f))
+
+
+def convert_book(filepath):
+    filename, file_extension = os.path.splitext(filepath)
+    if file_extension == ".txt":
+        # open both files
+        firstfile =  open(filepath,'r')
+        bookfile = open('./Sandbox/book.txt','a')
+        for line in firstfile:
+            secondfile.write(line)
+        close(filepath)
+        close(bookfile)
+    elif file_extension == ".epub":
+        book = epub2text(filepath)
+        f = open("Sandbox/book.txt", 'a')
+        for line in book:
+            f.write(line)
+        f.close
+    else:
+        print(file_extension)
+        print("File format not supported!")
+    name = filename.split("/")
+    return name[len(name) - 1]
+
+def split_chapterwise():
+    f = open("Sandbox/book.txt", "r")
+    entire_book = f.read()
+    text_splitter = "Chapter"
+    if "Chapter" in entire_book:
+        text_splitter = "Chapter"
+    elif "CHAPTER" in entire_book:
+        text_splitter = "CHAPTER"
+    chapters = entire_book.count(text_splitter)
+    print("Your book has " + str(chapters) + " chapters")
+    chaps = entire_book.split(text_splitter)
+    for i in range(len(chaps)):
+        chap = open("Sandbox/Chapter" + str(i) + ".txt", "a")
+        chap.write(chaps[i])
+        chap.close
+    print("Finished splitting your book into chapters")
+    return chapters
+
+
+def summarize_book(bookname, chapters):
+    os.mkdir("Summaries/" + bookname)
+    for i in range(chapters):
+        print("Summarizing Chapter " + str(i+1))
+        chapter = "Chapter" + str(i+1) + ".txt"
+        summarize_chapter("Sandbox/" + chapter, "Summaries/" + bookname  + "/"+ chapter)
+
+
+def summarize_chapter(inputpath, outputpath):
+    f = open(inputpath)
+    output = open(outputpath, "a")
+    text = f.read()
+    summary = text
+    summary = summarize(text)
+    output.write(summary)
+    f.close
+    output.close
+
+    output.write(summary)
+    print("Successfully done summarizing")
+
+def main():
+    if len(sys.argv) <= 1:
+        print("Please provide a file to summarize")
+        return
+    for i in range(1,len(sys.argv)):
+        empty_sandbox()
+        bookname = convert_book(sys.argv[i])
+        print("Beginning to summarize " + bookname)
+        chapters = split_chapterwise()
+        summarize_book(bookname, chapters)
+
+main()
